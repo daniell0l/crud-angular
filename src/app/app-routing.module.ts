15201ef70f695df7ea96f46ba2e9862f5ac15184
daniell0l <daniel_l0l@hotmail.com>
2023-08-02 02:19:17 -0300

    feat: fazendo referencia do path para pessoas e cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/' },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./cadastro/cadastro.module').then(m => m.CadastroModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/cadastro/cadastro-routing.module.ts
+++ src/app/cadastro/cadastro-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PessoasComponent } from './pessoas/pessoas.component';
+
+const routes: Routes = [
+  { path: '', component: PessoasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
